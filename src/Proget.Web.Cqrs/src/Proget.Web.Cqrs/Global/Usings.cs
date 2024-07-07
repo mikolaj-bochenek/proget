2d40220cffe259ea817d3cde78b1da3a47e756be
mikolaj-bochenek <mikolajbochenek.dev@gmail.com>
2024-07-07 09:56:53 +0200

    feat(web-cqrs): add put

--- src/Proget.Web.Cqrs/src/Proget.Web.Cqrs/Global/Usings.cs
+++ src/Proget.Web.Cqrs/src/Proget.Web.Cqrs/Global/Usings.cs
@@ -1,6 +1,7 @@
 global using Microsoft.AspNetCore.Routing;
 global using Microsoft.AspNetCore.Builder;
 global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
 
 global using FluentValidation;
 
---
+++ src/Proget.Web.Cqrs/src/Proget.Web.Cqrs/PutExtensions.cs
@@ -0,0 +1,35 @@
+namespace Proget.Web.Cqrs;
+
+public static class PutExtensions
+{
+    public static IEndpointRouteBuilder MapPutCqrs<TCommand, TResponse, TId>(
+        this IEndpointRouteBuilder builder,
+        string route,
+        Func<TCommand, TId, TCommand> preprocess,
+        Func<TCommand, TResponse> postprocess,
+        Action<RouteHandlerBuilder> configureEndpoint)
+        where TCommand : class, ICommand
+        where TResponse : class
+        where TId : notnull
+    {
+        var routeHandlerBuilder = builder.MapPut(route, async ([FromRoute] TId id, [FromBody] TCommand command, IValidator<TCommand> validator, ICommandDispatcher dispatcher, CancellationToken cancellationToken) =>
+        {
+            command = preprocess(command, id);
+
+            var validationResult = await validator.ValidateAsync(command, cancellationToken);
+            if (!validationResult.IsValid)
+            {
+                return Results.BadRequest(validationResult.Errors);
+            }
+            
+            await dispatcher.SendAsync(command, cancellationToken);
+            
+            var response = postprocess(command);
+            return Results.Ok(response);
+        });
+
+        configureEndpoint(routeHandlerBuilder);
+
+        return builder;
+    }
+}
